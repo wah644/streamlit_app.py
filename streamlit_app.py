387246b4f5af361a7f9d5730c31e5381f59ff03f
wah644 <wahajahmed644@gmail.com>
2025-01-24 00:47:49 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -223,7 +223,7 @@ if user_input:
         find_gene_match(GeneBe_results[2], 'HGNC:'+str(GeneBe_results[3]))
         
         # AI Tells me more
-        user_input = f"Tell me about the diseases: {disease_labels}. These were found to be linked to the following genetic variant: ACMG Classification: {GeneBe_results[0]}, Effect: {GeneBe_results[1]}, Gene Symbol: {GeneBe_results[2]}, Gene HGNC ID: {GeneBe_results[3]}"
+        user_input = f"Gene in interest: ACMG Classification: {GeneBe_results[0]}, Effect: {GeneBe_results[1]}, Gene Symbol: {GeneBe_results[2]}, Gene HGNC ID: {GeneBe_results[3]} The following diseases were found to be linked to the gene in interest: {disease_classification_dict}. Tell me about these diseases, ignore the diseases that have been refuted. "
         assistant_response = get_assistant_response(user_input)
         st.markdown(
         f"""
