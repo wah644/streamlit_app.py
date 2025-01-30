6c5cc135dde7ad3f05821a2cf6322a8c41de4c34
wah644 <wahajahmed644@gmail.com>
2025-01-30 13:48:32 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -292,16 +292,21 @@ if user_input:
         find_gene_match(GeneBe_results[2], 'HGNC:'+str(GeneBe_results[3]))
         
         # AI Tells me more
-        user_input_1 = f"The following diseases were found to be linked to the gene in interest: {disease_classification_dict}. Explain these diseases in depth, announce if a disease has been refuted, no need to explain that disease.if no diseases found reply with: No linked diseases found "
-        assistant_response_1 = get_assistant_response(user_input_1)
-        st.markdown(
-            f"""
-            <div class="justified-text">
-                Assistant: {assistant_response_1}
-            </div>
-            """,
-            unsafe_allow_html=True,
-        )
+        if "count" not in st.session_state:
+            st.session_state.count = 0
+
+        if st.session_state.count == 0:
+            user_input_1 = f"The following diseases were found to be linked to the gene in interest: {disease_classification_dict}. Explain these diseases in depth, announce if a disease has been refuted, no need to explain that disease.if no diseases found reply with: No linked diseases found "
+            assistant_response_1 = get_assistant_response_1(user_input_1)
+            st.markdown(
+                f"""
+                <div class="justified-text">
+                    Assistant: {assistant_response_1}
+                </div>
+                """,
+                unsafe_allow_html=True,
+            )
+            st.session_state.count += 1;
 
         
         #FINAL CHATBOT
