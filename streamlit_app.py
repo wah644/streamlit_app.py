7f2d3e05bc0b7bcfebceccaea1374513ff1c42f7
wah644 <wahajahmed644@gmail.com>
2025-02-01 13:43:58 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,7 +75,7 @@ def find_gene_match(gene_symbol, hgnc_id):
         matching_rows = df[(df['GENE SYMBOL'] == gene_symbol) & (df['GENE ID (HGNC)'] == hgnc_id)]
                 
         if not matching_rows.empty:
-            selected_columns = matching_rows[['DISEASE LABEL', 'MOI','CLASSIFICATION', 'DISEASE ID (MONDO)']].set_index('DISEASE LABEL')
+            selected_columns = matching_rows[['DISEASE LABEL', 'MOI','CLASSIFICATION', 'DISEASE ID (MONDO)']]
             st.write(selected_columns.style.apply(highlight_classification, axis=1))
             
             st.session_state.disease_classification_dict = dict(zip(matching_rows['DISEASE LABEL'], matching_rows['CLASSIFICATION']))
