316d22944015add0d745dfdeaa2cacd3335f5e9f
wah644 <wahajahmed644@gmail.com>
2025-02-01 12:07:31 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -299,6 +299,7 @@ if user_input != st.session_state.last_input:
         
         user_input_1 = f"The following diseases were found to be linked to the gene in interest: {disease_classification_dict}. Explain these diseases in depth, announce if a disease has been refuted, no need to explain that disease.if no diseases found reply with: No linked diseases found "
         reply = get_assistant_response_1(user_input_1)
+        st.write(reply)
            
                     
 
